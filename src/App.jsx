d8dfa410b3c924e8663a1c03b14f72a7a12873d4
MarcoEscobar1 <marco@example.com>
2026-02-12 22:04:16 -0400

    initial commint pasteleria

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-50">
+      <h1 className="text-4xl font-bold text-pink-700">
+        🧁 Landing Pastelería
+      </h1>
+    </div>
+  )
+}
+
+export default App
